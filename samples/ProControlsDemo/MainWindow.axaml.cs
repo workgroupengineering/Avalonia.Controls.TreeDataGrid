a9dfb2039157e2facece4a386f9129e6cbd0dd43
Takoooooo <dan.pan2012@yandex.ru>
2021-08-09 13:34:41 +0300

    add repro

--- samples/ProControlsDemo/MainWindow.axaml.cs
+++ samples/ProControlsDemo/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Avalonia;
 using Avalonia.Controls;
@@ -22,16 +23,39 @@ namespace ProControlsDemo
             this.AttachDevTools();
             Renderer.DrawFps = true;
             DataContext = new MainWindowViewModel();
-
+            this.Find<Button>("my").Click += MainWindow_Click;
             _tabs = this.FindControl<TabControl>("tabs");
-
-            DispatcherTimer.Run(() =>
-            {
-                UpdateRealizedCount();
-                return true;
-            }, TimeSpan.FromMilliseconds(500));
+            this.Find<TreeDataGrid>("fileViewer").CellPrepared += MainWindow_CellPrepared;
+            this.Find<Button>("remove").Click += MainWindow_Click1;
+            this.Find<Button>("add").Click += MainWindow_Click2;
+            //DispatcherTimer.Run(() =>
+            //{
+            //    UpdateRealizedCount();
+            //    return true;
+            //}, TimeSpan.FromMilliseconds(500));
 
             Activated += MainWindow_Activated;
+        }
+
+        private void MainWindow_Click2(object? sender, RoutedEventArgs e)
+        {
+            this.Find<DockPanel>("ss").Children.Add(this.Find<TreeDataGrid>("fileViewer"));
+        }
+
+        private void MainWindow_Click1(object? sender, RoutedEventArgs e)
+        {
+            this.Find<DockPanel>("ss").Children.Remove(this.Find<TreeDataGrid>("fileViewer"));
+        }
+
+        private void MainWindow_CellPrepared(object? sender, TreeDataGridCellEventArgs e)
+        {
+            var tt = (sender as TreeDataGrid).Rows[e.RowIndex];
+            var tt2 = (sender as TreeDataGrid).Columns[e.ColumnIndex];
+        }
+
+        private void MainWindow_Click(object? sender, RoutedEventArgs e)
+        {
+            (this.Find<TreeDataGrid>("fileViewer").Source as HierarchicalTreeDataGridSource<FileTreeNodeModel>).Items = new List<FileTreeNodeModel>();
         }
 
         private void MainWindow_Activated(object? sender, EventArgs e)
@@ -70,17 +94,17 @@ namespace ProControlsDemo
             ((MainWindowViewModel)DataContext!).Countries.AddCountry(country);
         }
 
-        private void UpdateRealizedCount()
-        {
-            var tabItem = (TabItem)_tabs.SelectedItem!;
-            var treeDataGrid = (TreeDataGrid)((Control)tabItem.Content).GetLogicalDescendants()
-                .FirstOrDefault(x => x is TreeDataGrid tl);
-            var textBlock = (TextBlock)((Control)tabItem.Content).GetLogicalDescendants()
-                .FirstOrDefault(x => x is TextBlock tb && tb.Classes.Contains("realized-count"));
-            var rows = treeDataGrid.RowsPresenter!;
-            var realizedRowCount = rows.RealizedElements.Count;
-            var unrealizedRowCount = ((ILogical)rows).LogicalChildren.Count - realizedRowCount;
-            textBlock.Text = $"{realizedRowCount} rows realized ({unrealizedRowCount} unrealized)";
-        }
+        //private void UpdateRealizedCount()
+        //{
+        //    var tabItem = (TabItem)_tabs.SelectedItem!;
+        //    var treeDataGrid = (TreeDataGrid)((Control)tabItem.Content).GetLogicalDescendants()
+        //        .FirstOrDefault(x => x is TreeDataGrid tl);
+        //    var textBlock = (TextBlock)((Control)tabItem.Content).GetLogicalDescendants()
+        //        .FirstOrDefault(x => x is TextBlock tb && tb.Classes.Contains("realized-count"));
+        //    var rows = treeDataGrid.RowsPresenter!;
+        //    var realizedRowCount = rows.RealizedElements.Count;
+        //    var unrealizedRowCount = ((ILogical)rows).LogicalChildren.Count - realizedRowCount;
+        //    textBlock.Text = $"{realizedRowCount} rows realized ({unrealizedRowCount} unrealized)";
+        //}
     }
 }
